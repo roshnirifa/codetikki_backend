7e9506c77cd13429e6737ae7fe79db8faa8a8cfa
roshnirifa <roshnirifa998@gmail.com>
2022-07-06 10:34:31 +0600

    jdoodle backend set

--- Backend/index.js
+++ Backend/index.js
@@ -1,46 +1,45 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
-const port = 5000
-app.use(express.urlencoded({ extended: true }));
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+// app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-// jdoodle api
-var request = require('request');
 
-var program = {
-    script: "",
-    language: "php",
-    versionIndex: "0",
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-};
-if (language == "c++") {
-    program.language = "cpp17";
-    program.versionIndex = "0";
-} else if (language == "c") {
-    program.language = "c99";
-    program.versionIndex = "4";
-} else if (language == "python") {
-    program.language = "python2";
-    program.versionIndex = "3";
-} else {
-    program.language = "java";
-    program.versionIndex = "0";
-}
-request({
-    url: 'https://api.jdoodle.com/v1/execute',
-    method: "POST",
-    json: program
-},
-    function (error, response, body) {
-        console.log('error:', error);
-        console.log('statusCode:', response && response.statusCode);
-        console.log('body:', body);
-    });
+app.post('/api', (req, res) => {
+    const code = req.body.code;
+
+    var request = require('request');
+
+    var program = {
+        script: code,
+        language: "cpp17",
+        versionIndex: "0",
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+    };
+
+    request({
+        url: 'https://api.jdoodle.com/v1/execute',
+        method: "POST",
+        json: program
+    },
+        function (error, response, body) {
+            console.log('error:', error);
+            console.log('statusCode:', response && response.statusCode);
+            console.log(code);
+            res.send({ body: body || error })
+        });
+
+})
+
+
 // checking the body api
 app.post("/run", (req, res) => {
     const { language = "cpp", code } = req.body
