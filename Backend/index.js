542e34873aceaf4a1fcb8341b3ee61c86d6405ed
roshnirifa <roshnirifa998@gmail.com>
2022-07-08 18:04:43 +0600

    python

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 require('dotenv').config()
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 // app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
@@ -13,7 +13,63 @@ app.get('/', (req, res) => {
 
 
 app.post('/api', (req, res) => {
-    const code = req.body.code;
+    let code = req.body.code;
+
+    var request = require('request');
+
+    var program = {
+        script: code,
+        language: "c",
+        versionIndex: "0",
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+    };
+
+    request({
+        url: 'https://api.jdoodle.com/v1/execute',
+        method: "POST",
+        json: program
+    },
+        function (error, response, body) {
+            console.log('error:', error);
+            console.log('statusCode:', response && response.statusCode);
+            console.log(code);
+            res.send({ body: body || error })
+        });
+
+})
+
+
+app.post('/apipython', (req, res) => {
+    let code = req.body.code;
+
+    var request = require('request');
+
+    var program = {
+        script: code,
+        language: "python3",
+        versionIndex: "0",
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+    };
+
+    request({
+        url: 'https://api.jdoodle.com/v1/execute',
+        method: "POST",
+        json: program
+    },
+        function (error, response, body) {
+            console.log('error:', error);
+            console.log('statusCode:', response && response.statusCode);
+            console.log(code);
+            res.send({ body: body || error })
+        });
+
+})
+
+
+app.post('/apicpp', (req, res) => {
+    let code = req.body.code;
 
     var request = require('request');
 
@@ -41,14 +97,14 @@ app.post('/api', (req, res) => {
 
 
 // checking the body api
-app.post("/run", (req, res) => {
-    const { language = "cpp", code } = req.body
-    if (code === undefined) {
-        return res.status(400).json({ success: false, error: "empty code body" })
-    }
-    return res.json({ language, code });
+// app.post("/run", (req, res) => {
+//     const { language = "cpp", code } = req.body
+//     if (code === undefined) {
+//         return res.status(400).json({ success: false, error: "empty code body" })
+//     }
+//     return res.json({ language, code });
 
-})
+// })
 
 app.listen(port,
     () => {
