5f454574dbd17b771491ed7673be622edb8b6988
roshnirifa <roshnirifa998@gmail.com>
2022-07-15 12:05:00 +0600

    steps

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 8000;
@@ -8,128 +9,141 @@ const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.post('/api', (req, res) => {
-    let code = req.body.code;
-
-    var request = require('request');
-
-    var program = {
-        script: code,
-        language: "c",
-        versionIndex: "0",
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-    };
-
-    request({
-        url: 'https://api.jdoodle.com/v1/execute',
-        method: "POST",
-        json: program
-    },
-        function (error, response, body) {
-            console.log('error:', error);
-            console.log('statusCode:', response && response.statusCode);
-            console.log(code);
-            res.send({ body: body || error })
-        });
-
-})
 
 
-app.post('/apipython', (req, res) => {
-    let code = req.body.code;
 
-    var request = require('request');
-
-    var program = {
-        script: code,
-        language: "python3",
-        versionIndex: "0",
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-    };
-
-    request({
-        url: 'https://api.jdoodle.com/v1/execute',
-        method: "POST",
-        json: program
-    },
-        function (error, response, body) {
-            console.log('error:', error);
-            console.log('statusCode:', response && response.statusCode);
-            console.log(code);
-            res.send({ body: body || error })
-        });
-
-})
-app.post('/apijava', (req, res) => {
-    let code = req.body.code;
-    var request = require('request');
-
-    var program = {
-        script: code,
-        language: "java",
-        versionIndex: "0",
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-    };
-
-    request({
-        url: 'https://api.jdoodle.com/v1/execute',
-        method: "POST",
-        json: program
-    },
-        function (error, response, body) {
-            console.log('error:', error);
-            console.log('statusCode:', response && response.statusCode);
-            console.log(code);
-            res.send({ body: body || error })
-        });
-
-})
+// Check the server
 
 
-app.post('/apicpp', (req, res) => {
-    let code = req.body.code;
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASS}@cluster0.dt7fdh2.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-    var request = require('request');
+async function run() {
+    try {
+        await client.connect();
+        console.log('db connect');
 
-    var program = {
-        script: code,
-        language: "cpp17",
-        versionIndex: "0",
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-    };
+        // const allProductCollection = client.db("clothesWareHouse").collection("allProducts");
 
-    request({
-        url: 'https://api.jdoodle.com/v1/execute',
-        method: "POST",
-        json: program
-    },
-        function (error, response, body) {
-            console.log('error:', error);
-            console.log('statusCode:', response && response.statusCode);
-            console.log(code);
-            res.send({ body: body || error })
-        });
+        app.get('/', (req, res) => {
+            res.send('Hello World!')
+        })
 
-})
+        app.post('/api', (req, res) => {
+            let code = req.body.code;
+
+            var request = require('request');
+
+            var program = {
+                script: code,
+                language: "c",
+                versionIndex: "0",
+                clientId: process.env.CLIENT_ID,
+                clientSecret: process.env.CLIENT_SECRET,
+            };
+
+            request({
+                url: 'https://api.jdoodle.com/v1/execute',
+                method: "POST",
+                json: program
+            },
+                function (error, response, body) {
+                    console.log('error:', error);
+                    console.log('statusCode:', response && response.statusCode);
+                    console.log(code);
+                    res.send({ body: body || error })
+                });
+
+        })
 
 
-// checking the body api
-// app.post("/run", (req, res) => {
-//     const { language = "cpp", code } = req.body
-//     if (code === undefined) {
-//         return res.status(400).json({ success: false, error: "empty code body" })
-//     }
-//     return res.json({ language, code });
+        app.post('/apipython', (req, res) => {
+            let code = req.body.code;
 
-// })
+            var request = require('request');
+
+            var program = {
+                script: code,
+                language: "python3",
+                versionIndex: "0",
+                clientId: process.env.CLIENT_ID,
+                clientSecret: process.env.CLIENT_SECRET,
+            };
+
+            request({
+                url: 'https://api.jdoodle.com/v1/execute',
+                method: "POST",
+                json: program
+            },
+                function (error, response, body) {
+                    console.log('error:', error);
+                    console.log('statusCode:', response && response.statusCode);
+                    console.log(code);
+                    res.send({ body: body || error })
+                });
+
+        })
+        app.post('/apijava', (req, res) => {
+            let code = req.body.code;
+            var request = require('request');
+
+            var program = {
+                script: code,
+                language: "java",
+                versionIndex: "0",
+                clientId: process.env.CLIENT_ID,
+                clientSecret: process.env.CLIENT_SECRET,
+            };
+
+            request({
+                url: 'https://api.jdoodle.com/v1/execute',
+                method: "POST",
+                json: program
+            },
+                function (error, response, body) {
+                    console.log('error:', error);
+                    console.log('statusCode:', response && response.statusCode);
+                    console.log(code);
+                    res.send({ body: body || error })
+                });
+
+        })
+
+
+        app.post('/apicpp', (req, res) => {
+            let code = req.body.code;
+
+            var request = require('request');
+
+            var program = {
+                script: code,
+                language: "cpp17",
+                versionIndex: "0",
+                clientId: process.env.CLIENT_ID,
+                clientSecret: process.env.CLIENT_SECRET,
+            };
+
+            request({
+                url: 'https://api.jdoodle.com/v1/execute',
+                method: "POST",
+                json: program
+            },
+                function (error, response, body) {
+                    console.log('error:', error);
+                    console.log('statusCode:', response && response.statusCode);
+                    console.log(code);
+                    res.send({ body: body || error })
+                });
+
+        })
+
+
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.listen(port,
     () => {
